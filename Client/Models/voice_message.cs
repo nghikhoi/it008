bd9a83f57948f7e75d632c66c2f307896418740f
Dcode-D <nhtdforwork@gmail.com>
2021-11-04 00:01:42 +0700

    Cap nhat models

---
+++ Client/Models/voice_message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Models
+{
+    class voice_message:message
+    {
+        public string id { get; private set; }
+        public voice_message(string id = "0") : base()
+        {
+            this.id = id;
+        }
+    }
+}
