30792ccea99e5eddcd67a9c99f5cb702ffc199f2
hiepchau <ch.dhiep@gmail.com>
2021-11-05 00:18:53 +0700

    update UI

---
+++ Client/ChangePassWord.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for ChangePassWord.xaml
+    /// </summary>
+    public partial class ChangePassWord : Window
+    {
+        public ChangePassWord()
+        {
+            InitializeComponent();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseOnDeactivated(object sender, EventArgs e)
+        {
+            Window win = sender as Window;
+            win.Close();
+        }
+    }
+}
