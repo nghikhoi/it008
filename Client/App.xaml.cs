b1c9ee7455adfad5af628948a52df75efb7a7086
Nghi Khôi <nghikhoi1108@gmail.com>
2021-11-11 00:31:55 +0700

    Add multi language

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows;
@@ -13,19 +14,37 @@ namespace UI
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
+
+        private readonly String defaultLanguage = "vi-VN";
+        private readonly List<String> availableLanguage = new List<string>() {
+            "en-US",
+            "vi-VN"
+        };
         
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             AppConfig.StartService();
+            ApplyLanguage(defaultLanguage);
             WindowLogIn logIn = new WindowLogIn();
             //MediaPlayerWindow main = new MediaPlayerWindow();
             //MainWindow main = new MainWindow();
             //TestWindows main = new TestWindows();
             //DownloadWindow main = new DownloadWindow();
             logIn.Show();
-           
+        }
+        
+        public void ApplyLanguage(string cultureName = null) {
+            if (cultureName != null)
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cultureName);
+
+            ResourceDictionary dict = new ResourceDictionary();
+            String langKey = Thread.CurrentThread.CurrentCulture.ToString();
+            if (!availableLanguage.Contains(langKey))
+                langKey = defaultLanguage;
+            dict.Source = new Uri("..\\Lang\\" + langKey + ".xaml", UriKind.Relative);
+
+            Resources.MergedDictionaries.Add(dict);
         }
 
     }
