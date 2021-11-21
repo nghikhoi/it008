094b056dd9e94e6b99fd56e2799317551f9238a4
Dcode-D <nhtdforwork@gmail.com>
2021-11-21 20:27:16 +0700

    Update close notiview on outside click

--- Client/MVC/ConversationList/ucListRecentMessage.xaml.cs
+++ Client/MVC/ConversationList/ucListRecentMessage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 using UI.MVC;
 
 namespace UI
@@ -139,6 +140,9 @@ namespace UI
                 window.Close();
             windowLogIn.Show();
         }
+
+
+
 
 
 
