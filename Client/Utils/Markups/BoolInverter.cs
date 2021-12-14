bf068303ce7258232f09e23bdfc2632371ea37cb
Nghi Khôi <nghikhoi1108@gmail.com>
2021-12-14 11:32:57 +0700

    Fix EmojiPicker

---
+++ Client/Utils/Markups/BoolInverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace UI.Utils.Markups {
+    public class BoolInverter : MarkupExtension, IValueConverter {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool b ? !b : value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool b ? !b : value;
+    }
+}
