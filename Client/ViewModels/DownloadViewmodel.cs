914de8822ea6fc1f2dfafa262acdb45788bcc6dc
Dcode-D <nhtdforwork@gmail.com>
2021-12-19 01:05:03 +0700

    Update download item

---
+++ Client/ViewModels/DownloadViewmodel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ViewModels
+{
+    class DownloadViewmodel:ViewModelBase
+    {
+        private string _fileName;
+        private double _downloadProgress;
+        public string FileName
+        {
+            get => _fileName;
+            set
+            {
+                OnPropertyChanged(nameof(FileName));
+                _fileName = value;
+            }
+        }
+
+        public Double DownloadProgress
+        {
+            get => _downloadProgress;
+            set
+            {
+                OnPropertyChanged(nameof(DownloadProgress));
+                _downloadProgress = value;
+            }
+        }
+        public DownloadViewmodel(string filename, double progress=0)
+        {
+            this.FileName = filename;
+            this.DownloadProgress = progress;
+        }
+
+    }
+}
