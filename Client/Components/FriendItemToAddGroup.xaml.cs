87d8aa813e684dae5c03e1dd127f62e972ea77ea
Dcode-D <nhtdforwork@gmail.com>
2021-12-19 03:13:21 +0700

    Update design of group adding and new friend item

---
+++ Client/Components/FriendItemToAddGroup.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI.Components
+{
+    /// <summary>
+    /// Interaction logic for FriendItemToAddGroup.xaml
+    /// </summary>
+    public partial class FriendItemToAddGroup : UserControl
+    {
+        public FriendItemToAddGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
