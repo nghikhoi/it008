c5267fd03df01ececd86fc744c8671ca47cc3aee
Dcode-D <nhtdforwork@gmail.com>
2021-12-19 13:45:05 +0700

    Announcement messages

---
+++ Client/ViewModels/Messages/AnnouncementViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using UI.Models.Message;
+using UI.Services;
+
+namespace UI.ViewModels.Messages
+{
+    class AnnouncementViewModel : MessageViewModel
+    {
+
+		public new TextMessage Message
+		{
+			get => (TextMessage)base.Message;
+			set => base.Message = value;
+		}
+
+		public string Text
+		{
+			get => Message.Message;
+			set
+			{
+				Message.Message = value;
+				OnPropertyChanged(nameof(Text));
+			}
+		}
+		public AnnouncementViewModel(IAppSession appSession) : base(appSession)
+		{
+
+		}
+	}
+}
